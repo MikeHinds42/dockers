15ce6cf154731d3177645eb1e9896a6f9085f31e
Mike Hinds <mike.hinds42@googlemail.com>
2019-01-22 21:11:43 +0000

    python3 and idea

--- idea/run.bat
+++ idea/run.bat
@@ -3,4 +3,6 @@
 
  :: docker run -it --rm --privileged -e DISPLAY=192.168.0.52:0 -v C:\Users\Mike\Documents\docker:/home/developer idea idea
 
-docker run --rm --privileged -e DISPLAY=192.168.0.52:0 -v C:\Users\Mike\Documents\docker:/home/developer idea idea
+docker run -it --rm --privileged -e DISPLAY=192.168.0.52:0 -v C:\Users\Mike\Documents\docker:/home/developer idea bash
+
+:: docker run -it --rm --privileged -e DISPLAY=192.168.0.52:0 --user root -v C:\Users\Mike\Documents\docker:/home/developer idea bash
